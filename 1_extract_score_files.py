013a04fb8f0796ec044153ece6f1b4500adf7a7c
Emma Luu <74997236+emmaluu314@users.noreply.github.com>
2024-10-07 17:53:01 -0700

    Update 1_extract_score_files.py
    The script was improved to be less cluttered and the typos were fixed

--- 1_extract_score_files.py
+++ 1_extract_score_files.py
@@ -4,17 +4,19 @@ import score_functions as sf
 # User defined variables:
 current_username = 'emmaluu'
 remote_server_name = 'cacao.genomecenter.ucdavis.edu'
-main_remove_server_path = '/share/siegellab/emmaluu/docking/template_for_automation/CAR/NMN/'
-main_folder_path = '/Users/emluu/Documents/Siegel lab/Rosetta Ligand/ARPA-E/CAR/Cousins temp/'
-target_list_path = main_folder_path + 'target_list.txt'
+main_server_path = '/share/siegellab/emmaluu/docking/' # main location of subfolders
+results_folder_name = "results/" #this is what your results folder is called. If you do not use a results folder, write "".
+main_local_path = '/Users/emluu/Documents/Siegel lab/Rosetta Ligand/'
+target_list_filename = 'master_list.txt' #name of file with a column of the subfolders you'd like to analyze
+target_list_path = main_local_path + target_list_filename
 
 
 def extract_scores_from_server(list_path, server_name, username, main_server_path, main_local_path):
     target_list = uf.compile_target_list(list_path)
-    sf.pull_from_cluster(target_list, server_name, username, main_server_path, main_local_path)
+    sf.pull_from_cluster(target_list, server_name, username, main_server_path, results_folder_name, main_local_path)
     # need to make it where I can have results flag
 
 if __name__ == '__main__':
     # need to make a flag that indicates whether there is a results subfolder
-    extract_scores_from_server(target_list_path, remote_server_name, current_username, main_remove_server_path,
-                               main_folder_path)
+    extract_scores_from_server(target_list_path, remote_server_name, current_username, main_server_path,
+                               main_local_path)
